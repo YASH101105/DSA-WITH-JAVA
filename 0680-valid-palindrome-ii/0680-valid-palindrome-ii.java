e232d4e668728d441ca4642c3903134747448528
YASH RASTOGI <rastogiy60@gmail.com>
2026-07-23 18:04:06 +0530

    Time: 4 ms (99.33%), Space: 48 MB (11.74%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s , int left , int right){
+        while(left<right){
+        if(s.charAt(left)!=s.charAt(right)){
+            return false;
+        }
+        left++;
+        right--;
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length()-1;
+        while(left<right){
+            if(s.charAt(left)==s.charAt(right)){
+                left++;
+                right--;
+            }
+            else{
+                boolean skipleft = isPalindrome(s, left+1 , right);
+                boolean skipright = isPalindrome(s, left , right-1);
+
+                return skipleft||skipright;
+            }
+        }
+        return true;
+        
+    }
+}
