a7d94e5bd7be131633ec716685607d60b2b84b7b
YASH RASTOGI <rastogiy60@gmail.com>
2026-08-22 16:43:01 +0530

    Time: 1 ms (97.52%), Space: 46.4 MB (38.37%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,18 +1,47 @@
 class Solution {
+
+    int index = 0;
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if(preorder.length==0){
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Store inorder value -> index
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+
+        return helper(preorder, 0, inorder.length - 1, map);
+    }
+
+    public TreeNode helper(int[] preorder, int left, int right,
+                           HashMap<Integer, Integer> map) {
+
+        // No elements
+        if (left > right) {
             return null;
         }
-        int r = preorder[0];
-        int index = 0;
-        for(int i = 0;i<inorder.length;i++){
-            if(inorder[i]==r){
-                index = i;
-            }
+
+        // Preorder gives root
+        int current = preorder[index];
+        index++;
+
+        TreeNode node = new TreeNode(current);
+
+        // Leaf node
+        if (left == right) {
+            return node;
         }
-        TreeNode root = new TreeNode(r);
-        root.left = buildTree(Arrays.copyOfRange(preorder, 1 , index+1),Arrays.copyOfRange(inorder, 0, index));
-        root.right = buildTree(Arrays.copyOfRange(preorder, index+1 , preorder.length),Arrays.copyOfRange(inorder, index+1, inorder.length));
-        return root;
+
+        // Find root position in inorder
+        int IncurrentIndex = map.get(current);
+
+        // Build left subtree
+        node.left = helper(preorder, left, IncurrentIndex - 1, map);
+
+        // Build right subtree
+        node.right = helper(preorder, IncurrentIndex + 1, right, map);
+
+        return node;
     }
 }
